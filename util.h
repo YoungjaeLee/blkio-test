a5a55827677ef4ed9ff75fba0c2cb882c4b01af6
Youngjae Lee <leeyo@us.ibm.com>
2017-01-11 14:04:56 -0600

    added rdtsc for x86 in util.h

--- util.h
+++ util.h
@@ -13,6 +13,13 @@ static __inline__ uint64_t getticks(void)
 
      return (((uint64_t)x0) << 32) | x;
 }
+#else
+static __inline__ uint64_t getticks(void)
+{
+	unsigned hi, lo;
+	__asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+	return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
+}
 #endif
 
 static __inline__ double elapsed(uint64_t t1, uint64_t t0)			
